b553df6cfbb1b1ca7274d15ed277adba8cf792c4
User <xalilovasilbek38@gmail.com>
2020-12-12 13:01:29 +0500

    Connect to the postgres database

--- alltoys/settings.py
+++ alltoys/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'alltoys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'all_toys',
+        'USER': 'postgres',
+        'PASSWORD': 'Asi$2001$0209',
     }
 }
 
