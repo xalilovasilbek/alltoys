fbe51c252cfa012421e4f34e0c5c453f7f011e61
User <xalilovasilbek38@gmail.com>
2020-12-14 12:20:00 +0500

    Add datetime field to Toy model

--- toys/models.py
+++ toys/models.py
@@ -35,3 +35,5 @@ class Toy(models.Model):
     user = models.ForeignKey(User, related_name='toys', on_delete=models.CASCADE, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     tags = models.ManyToManyField(Tag, related_name='toys')
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
